e2d6e92de203413ee95650fdaaad0bc1767f3619
PAMALSAHAN <43300747+PAMALSAHAN@users.noreply.github.com>
2019-07-23 23:51:11 +0530

    init vendor and item classes

--- shop.Designer.cs
+++ shop.Designer.cs
@@ -35,6 +35,7 @@
             this.shoppingListBox = new System.Windows.Forms.ListBox();
             this.storeItem = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // headerText
@@ -105,11 +106,21 @@
             this.button1.Text = "Purchase";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 397);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(212, 33);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // shop
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(660, 509);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.shoppingListBox);
             this.Controls.Add(this.storeItem);
@@ -136,6 +147,7 @@
         private System.Windows.Forms.ListBox shoppingListBox;
         private System.Windows.Forms.Label storeItem;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
